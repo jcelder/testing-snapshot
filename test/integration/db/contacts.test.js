472449ab22698e3c6881fb54e9904b39fa518ab5
Josh <aresx130@gmail.com>
2017-12-11 14:14:58 -0800

    Add tests for findById()

--- test/integration/db/contacts.test.js
+++ test/integration/db/contacts.test.js
@@ -7,7 +7,7 @@ const resetDB = require('../../helpers/db').resetContacts
 describe('src/models/db.js Integration Tests', () => {
   describe('findAll()', () => {
     beforeEach(() => resetDB())
-    it('should return 3 rows', () => {
+    it('should return 3 rows as an array', () => {
       return db.findAll()
         .then((results) => {
           expect(results).to.be.an('array').with.length(3)
@@ -22,6 +22,27 @@ describe('src/models/db.js Integration Tests', () => {
             last_name: 'Grippe',
           }
           expect(results[0]).to.be.an('object').that.deep.equals(testObject)
+        })
+    })
+  })
+  describe('findById(contactId)', () => {
+    const testId = 2
+    const testObject = {
+      id: 2,
+      first_name: 'Tanner',
+      last_name: 'Welsh',
+    }
+    beforeEach(() => resetDB())
+    it('should return 1 row as an object when given an id', () => {
+      return db.findById(testId)
+        .then((results) => {
+          expect(results).to.be.an('object')
+        })
+    })
+    it('should have a matching object when given an id', () => {
+      return db.findById(testId)
+        .then((results) => {
+          expect(results).to.deep.equal(testObject)
         })
     })
   })
