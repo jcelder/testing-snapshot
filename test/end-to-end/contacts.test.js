eb6724419fc98d28fb0954def46146267e0ebcc9
Josh <aresx130@gmail.com>
2017-12-12 12:53:44 -0800

    Add final end-to-end tests for 'contacts' routes
    * Add tests for 'contacts' POST route
    * Add tests for 'contacts/:contactid' routes
    * Add tests for 'contacts/search' route

--- test/end-to-end/contacts.test.js
+++ test/end-to-end/contacts.test.js
@@ -9,7 +9,8 @@ const app = require('../../src/server')
 const expect = chai.expect
 
 chai.use(chaiHttp)
-describe('End-to-End Tests', () => {
+describe('Contacts End-to-End Tests', () => {
+// Begin End-to-End Tests with Callbacks
   describe('"/" GET Route', () => {
     beforeEach(() => resetDB())
     it('should return a status of 200', (done) => {
@@ -56,21 +57,110 @@ describe('End-to-End Tests', () => {
         })
     })
   })
-
+  // Begin End-to-End Tests with Promises
   describe('"/contacts" POST Route', () => {
-
+    beforeEach(() => resetDB())
+    it('should return a status code of 200', () => {
+      return chai.request(app)
+        .post('/contacts')
+        .type('form')
+        .send({
+          _method: 'post',
+          first_name: 'Joshua',
+          last_name: 'Elder',
+        })
+        .then((res) => {
+          expect(res).to.have.status(200)
+        })
+    })
+    it('should insert a new contact into the database', () => {
+      return chai.request(app)
+        .post('/contacts')
+        .type('form')
+        .send({
+          _method: 'post',
+          first_name: 'Joshua',
+          last_name: 'Elder',
+        })
+        .then(() => {
+          return db.findById(4)
+            .then((newContact) => {
+              expect(newContact.first_name).to.equal('Joshua')
+            })
+        })
+    })
   })
 
   describe('"/contacts/:contactid" Routes', () => {
     describe('GET', () => {
-
+      beforeEach(() => resetDB())
+      it('should return status code 200', () => {
+        return chai.request(app)
+          .get('/contacts/1')
+          .then((res) => {
+            expect(res).to.have.status(200)
+          })
+      })
+      it('should render contact information for the specified ID', () => {
+        return chai.request(app)
+          .get('/contacts/1')
+          .then((res) => {
+            expect(res.text).to.include('<h1>Jared&nbsp;Grippe</h1>')
+          })
+      })
     })
     describe('DELETE', () => {
-
+      beforeEach(() => resetDB())
+      it('should return status code 200', () => {
+        return chai.request(app)
+          .del('/contacts/1')
+          .then((res) => {
+            expect(res).to.have.status(200)
+          })
+      })
+      it('should redirect to the "/" route', () => {
+        return chai.request(app)
+          .del('/contacts/1')
+          .then((res) => {
+            expect(res.text).to.include('<h1>Contacts</h1>')
+          })
+      })
+      it('should redirect to the "/" route with the contact removed', () => {
+        return chai.request(app)
+          .del('/contacts/1')
+          .then((res) => {
+            expect(res.text).to.not.include('Jared&nbsp;Grippe') 
+          })
+      })
     })
   })
 
   describe('"/search" Route', () => {
-
+    beforeEach(() => resetDB())
+    it('should return status code 200', () => {
+      return chai.request(app)
+        .get('/contacts/search')
+        .query({ q: 'Jared' })
+        .then((res) => {
+          expect(res).to.have.status(200)
+        })
+    })
+    it('should render the "contacts/index" page', () => {
+      return chai.request(app)
+        .get('/contacts/search')
+        .query({ q: 'Jared' })
+        .then((res) => {
+          expect(res.text).to.include('<h1>Contacts</h1>')
+        })
+    })
+    it('should only show results for the query', () => {
+      return chai.request(app)
+        .get('/contacts/search')
+        .query({ q: 'Jared' })
+        .then((res) => {
+          expect(res.text).to.not.include('NeEddra&nbsp;James')
+          expect(res.text).to.include('Jared&nbsp;Grippe')
+        })
+    })
   })
 })
